3ec7740d8e1be5e2b7b9fea7f10154427a93175b
Yamayaki <yamayaki.de@icloud.com>
2025-09-26 07:51:14 +0000

    Switch from Liberica to Microsoft JDK

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker image build -f java/ms17/Dockerfile ./java/
 docker image build -f java/ms21/Dockerfile ./java/
-docker image build -f java/lb25/Dockerfile ./java/
+docker image build -f java/ms25/Dockerfile ./java/
